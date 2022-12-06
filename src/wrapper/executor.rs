6d38ab46a912f1781bdaee314e18f043c64219bd
argv-minus-one <argv-minus-one@users.noreply.github.com>
2022-12-06 15:19:49 -0800

    Fix up some usages of `JNIEnv::with_local_frame` some more.

--- src/wrapper/executor.rs
+++ src/wrapper/executor.rs
@@ -74,9 +74,9 @@ impl Executor {
     {
         assert!(capacity > 0, "capacity should be a positive integer");
 
-        let jni_env = self.vm.attach_current_thread_as_daemon()?;
+        let mut jni_env = self.vm.attach_current_thread_as_daemon()?;
         let mut result = None;
-        jni_env.with_local_frame(capacity, |jni_env| {
+        jni_env.with_local_frame(capacity, |mut jni_env| {
             result = Some(f(&mut jni_env));
             Ok(JObject::null())
         })?;
